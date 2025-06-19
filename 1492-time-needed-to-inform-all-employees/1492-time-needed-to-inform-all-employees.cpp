9327c29b8576082e979de7eb70f308711362b3c2
MohitParashar06 <162456215+MohitParashar06@users.noreply.github.com>
2025-06-19 19:51:35 +0530

    Time: 11 ms (97.67%), Space: 97 MB (92.66%) - LeetHub

--- 1492-time-needed-to-inform-all-employees/1492-time-needed-to-inform-all-employees.cpp
+++ 1492-time-needed-to-inform-all-employees/1492-time-needed-to-inform-all-employees.cpp
@@ -1,15 +1,18 @@
 class Solution {
 public:
-    int f(vector<int>& manager, int node, vector<int>& it){
+    int f(vector<int>& manager, int node, vector<int>& it, vector<int>& dp){
         if(manager[node] == -1)
             return it[node];
-        return it[node]+f(manager,manager[node],it);
+        if(dp[node] != -1)
+            return dp[node];
+        return dp[node] = it[node]+f(manager,manager[node],it,dp);
     }
     int numOfMinutes(int n, int headID, vector<int>& manager, vector<int>& informTime) {
         int cnt = 0;
+        vector<int> dp(n+1,-1);
         for(int i = 0; i < informTime.size(); i++){
             if(informTime[i] == 0){
-                cnt = max(cnt,f(manager,i, informTime));
+                cnt = max(cnt,f(manager,i, informTime,dp));
             }
         }
         return cnt;
