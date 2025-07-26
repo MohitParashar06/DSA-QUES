4fff663a71ceb4abe1927907a0c20769d2a53a65
MohitParashar06 <162456215+MohitParashar06@users.noreply.github.com>
2025-07-26 21:53:58 +0530

    Time: 3 ms (67.52%), Space: 83.8 MB (57.23%) - LeetHub

---
+++ 3170-find-indices-with-index-and-value-difference-ii/3170-find-indices-with-index-and-value-difference-ii.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> findIndices(vector<int>& nums, int indexDifference, int valueDifference) {
+    int min_j = 0, max_j = 0;
+    for (int i = indexDifference, j = 0; i < nums.size(); ++i, ++j) {
+        min_j = nums[min_j] < nums[j] ? min_j : j;
+        max_j = nums[max_j] > nums[j] ? max_j : j;
+        if (abs(nums[i] - nums[min_j]) >= valueDifference)
+            return {min_j, i};
+        if (abs(nums[i] - nums[max_j]) >= valueDifference)
+            return {max_j, i};
+    }
+    return {-1, -1};
+}
+};
