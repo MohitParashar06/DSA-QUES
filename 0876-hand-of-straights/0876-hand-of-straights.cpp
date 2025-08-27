b3780cce203787e2a9903fc5a9146146762ecfa9
MohitParashar06 <162456215+MohitParashar06@users.noreply.github.com>
2025-08-27 22:43:05 +0530

    Time: 43 ms (38.07%), Space: 31.1 MB (82.96%) - LeetHub

---
+++ 0876-hand-of-straights/0876-hand-of-straights.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    bool isNStraightHand(vector<int>& hand, int gs) {
+        unordered_map<int,int> count;
+        for(int i : hand){
+            count[i]++;
+        }
+        sort(hand.begin(),hand.end());
+        int ans = 0;
+        int varCount = 0;
+        for(int i = 0; i < hand.size(); i++){
+            if(count[hand[i]] <= 0) continue;
+            int cnt = 1;
+            int x = hand[i];
+            count[x]--;
+            while(count[x+1] > 0 && cnt < gs){
+                // cout << x << " ";
+                cnt++;
+                count[x+1]--;
+                x++;
+            }
+            // cout << x << " ";
+            // cout << endl;
+            if(cnt == gs) ans++;
+            else return false;
+        }
+        return true;
+    }
+};
