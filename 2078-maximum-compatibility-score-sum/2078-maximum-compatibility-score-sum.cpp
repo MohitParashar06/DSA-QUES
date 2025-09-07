a2c666db761b4f88efd37194f7907a5534b59b82
MohitParashar06 <162456215+MohitParashar06@users.noreply.github.com>
2025-09-07 22:09:34 +0530

    Time: 3 ms (81.65%), Space: 13.7 MB (27.22%) - LeetHub

--- 2078-maximum-compatibility-score-sum/2078-maximum-compatibility-score-sum.cpp
+++ 2078-maximum-compatibility-score-sum/2078-maximum-compatibility-score-sum.cpp
@@ -1,9 +1,10 @@
 class Solution {
 public:
 
-    int f(vector<vector<int>>& st, vector<vector<int>>& mt, int ind, int mask){
+    int f(vector<vector<int>>& st, vector<vector<int>>& mt, int ind, int mask,vector<vector<int>> &dp){
         if(ind >= st.size()) return 0;
         int ans = 0;
+        if(dp[ind][mask] != -1) return dp[ind][mask];
         for(int i = 0; i < mt.size(); i++){
             int nmask = mask;
             if(nmask & (1 << i)){
@@ -11,15 +12,16 @@ public:
                 for(int j = 0; j < mt[i].size(); j++){
                     if(st[ind][j] == mt[i][j]) cnt++;
                 }
-                ans = max(ans,f(st,mt,ind+1,nmask ^ (1 << i))+cnt);
+                ans = max(ans,f(st,mt,ind+1,nmask ^ (1 << i),dp)+cnt);
             }
         }
-        return ans;
+        return dp[ind][mask] = ans;
     }
 
     int maxCompatibilitySum(vector<vector<int>>& students, vector<vector<int>>& mentors) {
         int m = mentors.size();
         int mask = (1 << m)-1;
-        return f(students,mentors,0,mask);
+        vector<vector<int>> dp(m, vector<int>(mask+1, -1));
+        return f(students,mentors,0,mask,dp);
     }
 };
